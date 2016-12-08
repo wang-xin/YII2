2871dab2886b2369d09f8c36be710455709548d3
DESKTOP-E0QJQHT\King <wangxin.king.314@gmail.com>
2016-12-08 17:40:33 +0800

    添加后台公共控制器

---
+++ backend/controllers/BaseController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 后台管理公共控制器.
+ * User: King
+ * Date: 2016/12/7
+ * Time: 19:21
+ */
+
+namespace backend\controllers;
+
+use common\controllers\CoreController;
+
+class BaseController extends CoreController
+{
+
+}
