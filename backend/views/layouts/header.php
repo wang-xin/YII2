22c113b7a6f93e10c26941b65b5d5facfb594c41
wangxin-king <wangxin.king.314@gmail.com>
2016-12-15 17:29:35 +0800

    修改管理员退出登录地址

--- backend/views/layouts/header.php
+++ backend/views/layouts/header.php
@@ -148,7 +148,7 @@
                                 <a href="#" class="btn btn-default btn-flat">Profile</a>
                             </div>
                             <div class="pull-right">
-                                <a href="<?= \yii\helpers\Url::to(['site/logout']) ?>" class="btn btn-default btn-flat">Sign out</a>
+                                <a href="<?= \yii\helpers\Url::to(['/site/logout']) ?>" class="btn btn-default btn-flat">Sign out</a>
                             </div>
                         </li>
                     </ul>
