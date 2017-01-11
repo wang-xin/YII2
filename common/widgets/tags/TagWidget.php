582f7c23f679b489f020a108710ea24b045ed566
wangxin-king <wangxin.king.314@gmail.com>
2017-01-11 16:44:04 +0800

    格式化代码

--- common/widgets/tags/TagWidget.php
+++ common/widgets/tags/TagWidget.php
@@ -1,5 +1,6 @@
 <?php
 namespace common\widgets\tags;
+
 /**
  * 标签云组件
  */
@@ -12,33 +13,33 @@ use common\widgets\tags\assets\TagAsset;
 class TagWidget extends InputWidget
 {
     public $value = '';
-    
+
     public function init()
     {
-        
+
     }
-    
+
     public function run()
     {
         $this->registerClientScript();
-        if($this->hasModel()){
+        if ($this->hasModel()) {
             $data['inputName'] = Html::getInputName($this->model, $this->attribute);
             $data['inputValue'] = Html::getAttributeValue($this->model, $this->attribute);
-        }else{
+        } else {
             $data['inputName'] = 'wd-tags';
             $data['inputValue'] = $this->value;
         }
-        
-        return $this->render('index',['data'=>$data]);
+
+        return $this->render('index', ['data' => $data]);
     }
-    
+
     /**
      * 注册Js
      */
     protected function registerClientScript()
     {
-         TagAsset::register($this->view);
-         $script = '$(function() {$("#select6").select2({ tags: true });});';
-         $this->view->registerJs($script, View::POS_END);
+        TagAsset::register($this->view);
+        $script = '$(function() {$("#select6").select2({ tags: true });});';
+        $this->view->registerJs($script, View::POS_END);
     }
 }
